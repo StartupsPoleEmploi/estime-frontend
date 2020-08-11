49ed2272a5535c620b2b5c7e3a9338bc21adcfee
Jérémie Lanceleur <jeremie.lanceleur@pole-emploi.fr>
2020-08-11 17:41:59 +0200

    dev homepage

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,9 @@
 export const environment = {
   production: true,
   apiEstimeURL: '/estime/v1/',
+  /** OpenID Connect PE properties ***/
+  oidcClientId: 'PAR_estime_92ac2bd6cc903ee96ac415bab999d13d29a0c1d71de408dc9246162ba99b002d',
+  oidcRedirectURI: 'https://estime.beta.pole-emploi.fr/',
+  oidcScope: 'application_PAR_estime_92ac2bd6cc903ee96ac415bab999d13d29a0c1d71de408dc9246162ba99b002d api_peconnect-individuv1 openid profile email api_peconnect-detailindemnisationsv1 detailindemnisation',
+  oidcPoleEmploiIdentityServerURL: 'https://authentification-candidat.pole-emploi.fr'
 };
