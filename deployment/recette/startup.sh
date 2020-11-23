cebc8542c46d27e5c820f76739e9584199f295f7
Jérémie Lanceleur <jeremie.lanceleur@pole-emploi.fr>
2020-11-23 20:54:53 +0100

    dev

--- deployment/recette/startup.sh
+++ deployment/recette/startup.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
 function replace_env_properties {
-	log 'replace tokens env properties in main.js'
-
-  perl -i -p -e "s|'%%tagcommander.script.url%%'|${TAG_COMMANDER_SCRIPT_URL}|g" usr/share/nginx/html/main*.js
-  perl -i -p -e "s|'%%pe-connect.redirect-uri%%'|${PE_CONNECT_REDIRECT_URI}|g" usr/share/nginx/html/main*.js
-  perl -i -p -e "s|'%%pe-connect.scope%%'|${PE_CONNECT_SCOPE}|g" usr/share/nginx/html/main*.js
-  perl -i -p -e "s|'%%pe-connect.identity-server-url%%'|${PE_CONNECT_IDENTITY_SERVER_URL}|g" usr/share/nginx/html/main*.js
+  log 'replace tokens env properties in main.js'
+  perl -i -p -e "
+  $PE_CONNECT_CLIENT_ID=qw('$PE_CONNECT_CLIENT_ID');
+  $PE_CONNECT_REDIRECT_URI=qw('$PE_CONNECT_REDIRECT_URI');
+  $PE_CONNECT_SCOPE=qw('$PE_CONNECT_SCOPE');
+  $PE_CONNECT_IDENTITY_SERVER_URL=qw('$PE_CONNECT_IDENTITY_SERVER_URL');
+  s|pe-connect.client-id|$PE_CONNECT_CLIENT_ID|g;
+  s|pe-connect.redirect-uri|$PE_CONNECT_REDIRECT_URI|g;
+  s|pe-connect.scope|$PE_CONNECT_SCOPE|g;
+  s|pe-connect.identity-server-url|$PE_CONNECT_IDENTITY_SERVER_URL|g" usr/share/nginx/html/main*.js
 }
+
 
 function replace_tagCommander_url {
 	log 'replace tagcommander script url in index.html'
 
-	perl -i -p -e "s|'%%tagcommander.script.url%%'|${TAG_COMMANDER_SCRIPT_URL}|g" usr/share/nginx/html/index.html
+	perl -i -p -e "s|'tagcommander.script.url'|${TAG_COMMANDER_SCRIPT_URL}|g" usr/share/nginx/html/index.html
 }
 
 start_nginx() {
@@ -22,8 +27,8 @@ start_nginx() {
 
 function start {
   replace_env_properties &&
-	replace_tagCommander_url &&
-	start_nginx
+  replace_tagCommander_url &&
+  start_nginx
 }
 
 function log {
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,9 +2,9 @@ export const environment = {
   production: true,
   apiEstimeURL: '/estime/v1/',
   /** OpenID Connect PE properties ***/
-  peconnectClientid: '%%pe-connect.client-id%%',
-  peconnectRedirecturi: '%%pe-connect.redirect-uri%%',
-  peconnectScope: '%%pe-connect.scope%%',
-  peconnectIdentityServerURL: '%%pe-connect.identity-server-url%%',
-  tagCommanderScriptUrl: '%%tagcommander.script.url%%'
+  peconnectClientid: 'pe-connect.client-id',
+  peconnectRedirecturi: 'pe-connect.redirect-uri',
+  peconnectScope: 'pe-connect.scope',
+  peconnectIdentityServerURL: 'pe-connect.identity-server-url',
+  tagCommanderScriptUrl: 'tagcommander.script.url'
 };
