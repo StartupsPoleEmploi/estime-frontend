8ee89b90f9d6ae98a67de012d63454044f9c8022
Jérémie Lanceleur <jeremie.lanceleur@pole-emploi.fr>
2020-11-23 21:34:16 +0100

    dev

--- deployment/recette/startup.sh
+++ deployment/recette/startup.sh
@@ -3,17 +3,17 @@
 function replace_env_properties {
   log 'replace tokens env properties in main.js'
 
-  perl -i -p -e "s|'pe-connect.client-id'|${PE_CONNECT_CLIENT_ID}|g" usr/share/nginx/html/main*.js
-  perl -i -p -e "s|'pe-connect.redirect-uri'|${PE_CONNECT_REDIRECT_URI}|g" usr/share/nginx/html/main*.js
-  perl -i -p -e "s|'pe-connect.scope'|${PE_CONNECT_SCOPE}|g" usr/share/nginx/html/main*.js
-  perl -i -p -e "s|'pe-connect.identity-server-url'|${PE_CONNECT_IDENTITY_SERVER_URL}|g" usr/share/nginx/html/main*.js
+  perl -i -p -e "s|pe-connect.client-id|${PE_CONNECT_CLIENT_ID}|g" usr/share/nginx/html/main*.js
+  perl -i -p -e "s|pe-connect.redirect-uri|${PE_CONNECT_REDIRECT_URI}|g" usr/share/nginx/html/main*.js
+  perl -i -p -e "s|pe-connect.scope|${PE_CONNECT_SCOPE}|g" usr/share/nginx/html/main*.js
+  perl -i -p -e "s|pe-connect.identity-server-url|${PE_CONNECT_IDENTITY_SERVER_URL}|g" usr/share/nginx/html/main*.js
 }
 
 
 function replace_tagCommander_url {
 	log 'replace tagcommander script url in index.html'
 
-	perl -i -p -e "s|'tagcommander.script.url'|${TAG_COMMANDER_SCRIPT_URL}|g" usr/share/nginx/html/index.html
+	perl -i -p -e "s|tagcommander.script.url|${TAG_COMMANDER_SCRIPT_URL}|g" usr/share/nginx/html/index.html
 }
 
 start_nginx() {
