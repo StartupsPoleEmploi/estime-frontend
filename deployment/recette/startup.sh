e709a4884f644afb0de544040e2d771691d65b80
Jérémie Lanceleur <jeremie.lanceleur@pole-emploi.fr>
2020-11-23 20:10:20 +0100

    dev

--- deployment/recette/startup.sh
+++ deployment/recette/startup.sh
@@ -3,15 +3,10 @@
 function replace_env_properties {
 	log 'replace tokens env properties in main.js'
 
-  perl -i -p -e '
-	  $PE_CONNECT_CLIENT_ID=qw('$PE_CONNECT_CLIENT_ID');
-	  $PE_CONNECT_REDIRECT_URI=qw('$PE_CONNECT_REDIRECT_URI');
-    $PE_CONNECT_SCOPE=qw('$PE_CONNECT_SCOPE');
-    $PE_CONNECT_IDENTITY_SERVER_URL=qw('$PE_CONNECT_IDENTITY_SERVER_URL');
-	  s|%%pe-connect.client-id%%|$PE_CONNECT_CLIENT_ID|g;
-	  s|%%pe-connect.redirect-uri%%|$PE_CONNECT_REDIRECT_URI|g;
-    s|%%pe-connect.scope%%|$PE_CONNECT_SCOPE|g;
-	  s|%%pe-connect.identity-server-url%%|$PE_CONNECT_IDENTITY_SERVER_URL|g' usr/share/nginx/html/main*.js
+  perl -i -p -e "s|'%%tagcommander.script.url%%'|${TAG_COMMANDER_SCRIPT_URL}|g" usr/share/nginx/html/main*.js
+  perl -i -p -e "s|'%%pe-connect.redirect-uri%%'|${PE_CONNECT_REDIRECT_URI}|g" usr/share/nginx/html/main*.js
+  perl -i -p -e "s|'%%pe-connect.scope%%'|${PE_CONNECT_SCOPE}|g" usr/share/nginx/html/main*.js
+  perl -i -p -e "s|'%%pe-connect.identity-server-url%%'|${PE_CONNECT_IDENTITY_SERVER_URL}|g" usr/share/nginx/html/main*.js
 }
 
 function replace_tagCommander_url {
