dbcf7fc4103afb086d227cf51255686e67d3573a
Jérémie Lanceleur <jeremie.lanceleur@pole-emploi.fr>
2020-11-24 11:23:36 +0100

    dev

--- docker/commun/scripts/startup.sh
+++ docker/commun/scripts/startup.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-function replace_env_properties {
-  log 'replace tokens env properties in main.js'
-
+function replace_properties_in_main {
+  log 'replace properties in main.js'
   perl -i -p -e "s|pe-connect.client-id|${PE_CONNECT_CLIENT_ID}|g" usr/share/nginx/html/main*.js
   perl -i -p -e "s|pe-connect.redirect-uri|${PE_CONNECT_REDIRECT_URI}|g" usr/share/nginx/html/main*.js
   perl -i -p -e "s|pe-connect.scope|${PE_CONNECT_SCOPE}|g" usr/share/nginx/html/main*.js
   perl -i -p -e "s|pe-connect.identity-server-url|${PE_CONNECT_IDENTITY_SERVER_URL}|g" usr/share/nginx/html/main*.js
 }
 
-function replace_tagCommander_url {
-	log 'replace tagcommander script url in index.html'
-
+function replace_properties_in_index {
+	log 'replace properties in index.html'
 	perl -i -p -e "s|tagcommander.script.url|${TAG_COMMANDER_SCRIPT_URL}|g" usr/share/nginx/html/index.html
 }
 
@@ -21,8 +19,8 @@ function start_nginx {
 }
 
 function start {
-  replace_env_properties &&
-  replace_tagCommander_url &&
+  replace_properties_in_main &&
+  replace_properties_in_index &&
   start_nginx
 }
 
