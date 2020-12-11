ec1de36bee7352d84fe68fc62f23d5b116b4da42
Jérémie Lanceleur <jeremie.lanceleur@pole-emploi.fr>
2020-12-11 18:01:38 +0100

    fix css

--- src/app/commun/enumerations/situations-personne.enum.ts
+++ src/app/commun/enumerations/situations-personne.enum.ts
@@ -1,7 +1,7 @@
 export enum SituationPersonneEnum {
-  AAH = "L'AAH (Allocation Adulte Handicapé)",
-  ASS ="L'ASS (Allocation Solidarité Spécifique)",
-  ARE = "L'ARE (Allocation Retour Emploi)",
+  AAH = "L' AAH (Allocation Adulte Handicapé)",
+  ASS ="L' ASS (Allocation Solidarité Spécifique)",
+  ARE = "L' ARE (Allocation Retour Emploi)",
   BENEFICIAIRE_REVENUS_IMMOBILIER = "Des revenus immobilier",
   CREATEUR_ENTREPRISE = "Des revenus en tant que travailleur indépendant",
   RSA = "Le RSA (Revenu Solidarité Active)",
