202b7f9b787903099efbc1149fb06a7535b1b930
Jérémie Lanceleur <jeremie.lanceleur@pole-emploi.fr>
2020-12-16 17:32:58 +0100

    dev e2e

--- cypress/integration/spec.ts
+++ cypress/integration/spec.ts
@@ -1,11 +1,28 @@
 const specTitle = require("cypress-sonarqube-reporter/specTitle");
 
-describe(specTitle('En tant qu\'utilisateur, lorsque j\'accède au service Estime sur https//estime.beta.pole-emploi.fr'), () => {
+describe(specTitle('US - accès au service Estime'), () => {
+
+
   beforeEach(() => {
     cy.visit('/');
   });
 
-  it('alors la page d\'accueil s\'affiche.', () => {
-    cy.contains('Estime calcule le montant de vos allocations et des aides auxquelles vous pourriez prétendre.');
+  it('En tant que demandeur d\'emploi, je souhaite me connecter au service Estime en utilisant mon compte Pôle emploi', () => {
+
+    cy.get('button[type="button"]').click();
+
+    cy.wait(3000);
+
+    cy.get('input[id="identifiant"]').type("CaroASS");
+    cy.get('button[id="submit"]').click();
+    cy.wait(3000);
+    cy.get('input[id="password"]').type("Muscoli.1");
+    cy.get('button[id="submit"]').click();
+
+    cy.contains('Avant de commencer');
   });
 });
+
+
+
+
