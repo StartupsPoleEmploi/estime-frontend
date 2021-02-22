4abd1e22fb366af7ea497e49bb87e813dfed85fe
Jérémie Lanceleur <jeremie.lanceleur@pole-emploi.fr>
2021-02-22 08:42:36 +0100

    dev

--- src/app/public/public-routing.module.ts
+++ src/app/public/public-routing.module.ts
@@ -5,12 +5,14 @@ import { SigninRedirectCallbackComponent } from '@app/commun/components/signin-r
 import { SignoutRedirectCallbackComponent } from '@app/commun/components/signout-redirect-callback.component';
 import { RoutesEnum } from '@app/commun/enumerations/routes.enum';
 import { CguComponent } from './cgu/cgu.component';
+import { ContactComponent } from './contact/contact.component';
 import { HomepageComponent } from './homepage/homepage.component';
 
 
 const routes: Routes = [
   {path: '', component: HomepageComponent, pathMatch: 'full' },
   {path: RoutesEnum.CGU, component: CguComponent },
+  {path: RoutesEnum.CONTACT, component: ContactComponent },
   {path: RoutesEnum.SIGNIN_CALLBACK, component: SigninRedirectCallbackComponent },
   {path: RoutesEnum.SIGNOUT_CALLBACK, component: SignoutRedirectCallbackComponent },
   {path: '404', component: NotFoundComponent},
