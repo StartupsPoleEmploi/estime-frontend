b0fc462f8255636cdd163e3e00efabb5bcd1760e
Jérémie Lanceleur <jeremie.lanceleur@pole-emploi.fr>
2021-05-10 21:18:50 +0200

    refacto

--- docker/commun/scripts/deploy.sh
+++ docker/commun/scripts/deploy.sh
@@ -2,7 +2,15 @@
 echo "$3" | docker login -u $2 --password-stdin $1
 
 #deploy a new stack or update an existing stack
-docker stack deploy --with-registry-auth -c estime-frontend-stack.yml estime-frontend
+container_name=estime-frontend
+# if new version is passed and container exists, update estime-frontend image
+if [ ! -z "$4" ] && [sudo docker container ls -a --format '{{.Names}}' | grep -Eq "^${container_name}\$"]; then
+  echo 'coucou1'
+  docker service update --image registry.beta.pole-emploi.fr/estime/estime-frontend:$4 estime-frontend
+else
+  echo 'coucou2'
+  docker stack deploy --with-registry-auth -c estime-frontend-stack.yml estime-frontend
+fi
 
 #clean docker environment
 docker image prune --force
