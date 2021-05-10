dd44598a7d3377fbdb8caea3a62274283aaf7959
Jérémie Lanceleur <jeremie.lanceleur@pole-emploi.fr>
2021-05-10 21:43:41 +0200

    conf nginx

--- docker/commun/scripts/deploy.sh
+++ docker/commun/scripts/deploy.sh
@@ -2,15 +2,7 @@
 echo "$3" | docker login -u $2 --password-stdin $1
 
 #deploy a new stack or update an existing stack
-container_name=estime-frontend
-# if new version is passed and container exists, update estime-frontend image
-if [ ! -z "$4" ] && [sudo docker container ls -a --format '{{.Names}}' | grep -Eq "^${container_name}\$"]; then
-  echo 'coucou1'
-  docker service update --image registry.beta.pole-emploi.fr/estime/estime-frontend:$4 estime-frontend
-else
-  echo 'coucou2'
-  docker stack deploy --with-registry-auth -c estime-frontend-stack.yml estime-frontend
-fi
+docker stack deploy --with-registry-auth -c estime-frontend-stack.yml estime-frontend
 
 #clean docker environment
 docker image prune --force
