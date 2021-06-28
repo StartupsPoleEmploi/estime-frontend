59dafc1a2d7a39433980a1ded9180cd9f9defbc2
ldetre <lucas.detre@gmail.com>
2021-06-28 11:27:59 +0200

    US 90 page 404 ok, US 112 déselection pavés ass rsa ok

--- src/app/protected/etapes-simulation/2.ma-situation/ma-situation.component.ts
+++ src/app/protected/etapes-simulation/2.ma-situation/ma-situation.component.ts
@@ -86,18 +86,12 @@ export class MaSituationComponent implements OnInit {
   public onClickCheckBoxHasASS(): void {
     if (!this.beneficiaireAidesSociales.beneficiaireASS) {
       this.deConnecteService.unsetAllocationMensuelleNetASS();
-    } else {
-      this.deConnecteService.unsetInfosRSA();
-      this.beneficiaireAidesSociales.beneficiaireRSA = false;
     }
   }
 
   public onClickCheckBoxHasRSA(): void {
     if (!this.beneficiaireAidesSociales.beneficiaireRSA) {
       this.deConnecteService.unsetInfosRSA();
-    } else {
-      this.deConnecteService.unsetAllocationMensuelleNetASS();
-      this.beneficiaireAidesSociales.beneficiaireASS = false;
     }
   }
 
