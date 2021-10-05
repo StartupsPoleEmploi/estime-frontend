09114e578782f7a5d08605110ac4deaac3584591
ldetre <lucas.detre@gmail.com>
2021-10-05 08:37:23 +0200

    ajout page stats

---
+++ src/app/public/stats/stats.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { ScreenService } from '@app/core/services/utile/screen.service';
+
+@Component({
+  selector: 'app-stats',
+  templateUrl: './stats.component.html',
+  styleUrls: ['./stats.component.scss']
+})
+export class StatsComponent implements OnInit {
+
+  constructor(
+    public screenService: ScreenService
+  ) { }
+
+  ngOnInit(): void {
+    window.location.href="https://nextcloud.beta.pole-emploi.fr/index.php/apps/onlyoffice/s/5BZTDkwKqtWpXCA";
+  }
+
+}
