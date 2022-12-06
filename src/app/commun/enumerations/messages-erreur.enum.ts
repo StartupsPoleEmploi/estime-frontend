03b707c09b9b3f35dabe11206ad6a3c85a3537b1
ldetre <lucas.detre@gmail.com>
2022-12-06 21:25:08 +0100

    US285 : retirer visuellement l'identité d'Estime

--- src/app/commun/enumerations/messages-erreur.enum.ts
+++ src/app/commun/enumerations/messages-erreur.enum.ts
@@ -1,9 +1,9 @@
 export enum MessagesErreurEnum {
-  CONNEXION_ESTIME_IMPOSSIBLE = "La connexion au service a échoué. Merci d'essayer de nouveau et de contacter le support Estime si le problème persiste.",
-  ERREUR_SERVICE = "Erreur du service, merci d'essayer de nouveau et de contacter le support Estime si le problème persiste.",
-  SIMULATION_IMPOSSIBLE = "Impossible d'obtenir votre simulation. Merci d'essayer de nouveau et de contacter le support Estime si le problème persiste.",
-  SUPPRESSION_DONNEES_PERSO_IMPOSSIBLE = "Impossible de supprimer vos données personnelles. Merci d'essayer de nouveau et de contacter le support Estime si le problème persiste.",
-  SIMULATION_IMPOSSIBLE_PENSION_INVALIDITE_ET_SALAIRES = "Estime n'est pas en mesure d'effectuer la simulation au regard de votre situation, merci de vous renseigner auprès de l'organisme en charge du paiement de votre pension d'invalidité.",
-  ERREUR_RECUPERATION_AIDES = "Impossible d'afficher les détail de cette aide. Merci d'essayer de nouveau et de contacter le support Estime si le problème persiste.",
-  ERREUR_ENREGISTREMENT_AUTRES_SITUATIONS = "Impossible d'enregister votre situation. Merci d'essayer de nouveau et de contacter le support Estime si le problème persiste."
+  CONNEXION_ESTIME_IMPOSSIBLE = "La connexion au service a échoué. Merci d'essayer de nouveau et de contacter le support si le problème persiste.",
+  ERREUR_SERVICE = "Erreur du service, merci d'essayer de nouveau et de contacter le support si le problème persiste.",
+  SIMULATION_IMPOSSIBLE = "Impossible d'obtenir votre simulation. Merci d'essayer de nouveau et de contacter le support si le problème persiste.",
+  SUPPRESSION_DONNEES_PERSO_IMPOSSIBLE = "Impossible de supprimer vos données personnelles. Merci d'essayer de nouveau et de contacter le support si le problème persiste.",
+  SIMULATION_IMPOSSIBLE_PENSION_INVALIDITE_ET_SALAIRES = "Nous ne sommes pas en mesure d'effectuer la simulation au regard de votre situation, merci de vous renseigner auprès de l'organisme en charge du paiement de votre pension d'invalidité.",
+  ERREUR_RECUPERATION_AIDES = "Impossible d'afficher les détail de cette aide. Merci d'essayer de nouveau et de contacter le support si le problème persiste.",
+  ERREUR_ENREGISTREMENT_AUTRES_SITUATIONS = "Impossible d'enregister votre situation. Merci d'essayer de nouveau et de contacter le support si le problème persiste."
 }
