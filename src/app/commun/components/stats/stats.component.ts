d44fc052769257827438286a76ca0db559249a0b
ldetre <lucas.detre@gmail.com>
2022-12-13 19:49:50 +0100

    Fix

--- src/app/commun/components/stats/stats.component.ts
+++ src/app/commun/components/stats/stats.component.ts
@@ -15,5 +15,4 @@ export class StatsComponent implements OnInit {
   ngOnInit(): void {
     window.location.href = "https://datastudio.google.com/reporting/f7fae1db-bfde-4c18-ab2e-cd1da8e9917a/page/8l8gC";
   }
-
 }
